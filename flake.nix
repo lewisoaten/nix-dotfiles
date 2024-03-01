7aa95952dc622bc137cb97989d47b154c928efdf
Lewis Oaten <lewis@oaten.name>
2024-03-01 23:15:08 +0000

    Update flake.nix to 23.11 not unstable

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-howdy.url = "github:fufexan/nixpkgs/howdy";
 
     flake-parts = {
